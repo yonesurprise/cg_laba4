cb8dc92de192f214fd97b3633406385656cda3a3
yonesurprise <yonesurprise@gmail.com>
2022-11-08 00:51:35 +0300

    add point, edge, polygon, clear

--- laba4/Form1.Designer.cs
+++ laba4/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace laba4
 {
-    partial class Form1
+    partial class Laba3
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,117 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton_polygon = new System.Windows.Forms.RadioButton();
+            this.radioButton_otrezok = new System.Windows.Forms.RadioButton();
+            this.radioButton_point = new System.Windows.Forms.RadioButton();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(11, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(652, 607);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.groupBox1.Controls.Add(this.radioButton_polygon);
+            this.groupBox1.Controls.Add(this.radioButton_otrezok);
+            this.groupBox1.Controls.Add(this.radioButton_point);
+            this.groupBox1.Location = new System.Drawing.Point(667, 11);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(183, 45);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // radioButton_polygon
+            // 
+            this.radioButton_polygon.AutoSize = true;
+            this.radioButton_polygon.Location = new System.Drawing.Point(117, 17);
+            this.radioButton_polygon.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton_polygon.Name = "radioButton_polygon";
+            this.radioButton_polygon.Size = new System.Drawing.Size(63, 17);
+            this.radioButton_polygon.TabIndex = 2;
+            this.radioButton_polygon.TabStop = true;
+            this.radioButton_polygon.Text = "Polygon";
+            this.radioButton_polygon.UseVisualStyleBackColor = true;
+            this.radioButton_polygon.CheckedChanged += new System.EventHandler(this.radioButton_polygon_CheckedChanged);
+            // 
+            // radioButton_otrezok
+            // 
+            this.radioButton_otrezok.AutoSize = true;
+            this.radioButton_otrezok.Location = new System.Drawing.Point(63, 17);
+            this.radioButton_otrezok.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton_otrezok.Name = "radioButton_otrezok";
+            this.radioButton_otrezok.Size = new System.Drawing.Size(50, 17);
+            this.radioButton_otrezok.TabIndex = 1;
+            this.radioButton_otrezok.TabStop = true;
+            this.radioButton_otrezok.Text = "Edge";
+            this.radioButton_otrezok.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_point
+            // 
+            this.radioButton_point.AutoSize = true;
+            this.radioButton_point.Location = new System.Drawing.Point(4, 17);
+            this.radioButton_point.Margin = new System.Windows.Forms.Padding(2);
+            this.radioButton_point.Name = "radioButton_point";
+            this.radioButton_point.Size = new System.Drawing.Size(49, 17);
+            this.radioButton_point.TabIndex = 0;
+            this.radioButton_point.TabStop = true;
+            this.radioButton_point.Text = "Point";
+            this.radioButton_point.UseVisualStyleBackColor = true;
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.clearButton.Location = new System.Drawing.Point(671, 60);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(74, 26);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Laba3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1258, 629);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Laba3";
+            this.Text = "Laba3";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton_polygon;
+        private System.Windows.Forms.RadioButton radioButton_otrezok;
+        private System.Windows.Forms.RadioButton radioButton_point;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- laba4/Form1.cs
+++ laba4/Form1.cs
@@ -3,18 +3,127 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TreeView;
 
 namespace laba4
 {
-    public partial class Form1 : Form
+    public partial class Laba3 : Form
     {
-        public Form1()
+        Bitmap pic;
+        Graphics graph;
+        List<Point> points = new List<Point>();
+        List<(Point, Point)> lines = new List<(Point, Point)>();
+        List<List<Point>> polygons = new List<List<Point>>();
+        Pen pen = new Pen(Color.Black, 1), rpen = new Pen(Color.Red, 5);
+        bool first = false;
+        bool cur_pol = false;
+        (int, int) first_coord;
+        int point_count = 0;
+        int polygon_count = 0;
+        int lines_count = 0;
+        string curpol = "";
+        string curpoint = "";
+        string curline1 = "";
+        string curline2 = "";
+
+        public Laba3()
         {
             InitializeComponent();
+            pic = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = pic;
+            graph = Graphics.FromImage(pictureBox1.Image);
+            graph.Clear(Color.White);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            first = false;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton_point.Checked)
+            {
+                points.Add(new Point(e.X, e.Y));
+            }
+            else if (radioButton_otrezok.Checked)
+            {
+                if (!first)
+                {
+                    first_coord = (e.X, e.Y);
+                    first = true;
+                }
+                else
+                {
+                    lines.Add((new Point(first_coord.Item1, first_coord.Item2), new Point(e.X, e.Y)));
+                    first = false;
+                }
+            }
+            else if (radioButton_polygon.Checked)
+            {
+                polygons[polygons.Count - 1].Add(new Point(e.X, e.Y));
+                if (!cur_pol)
+                {
+                    cur_pol = true;
+                }
+            }
+            DrawAll();
+        }
+
+        void DrawAll()
+        {
+            graph.Clear(Color.White);
+            foreach (var x in points)
+                pic.SetPixel(x.X, x.Y, Color.Black);
+            pen.EndCap = LineCap.NoAnchor;
+            foreach (var x in lines)
+                graph.DrawLine(pen, x.Item1.X, x.Item1.Y, x.Item2.X, x.Item2.Y);
+            pen.EndCap = LineCap.NoAnchor;
+            foreach (var x in polygons)
+            {
+                graph.DrawLine(pen, x[0], x[x.Count - 1]);
+                for (int i = 0; i < x.Count - 1; i++)
+                    graph.DrawLine(pen, x[i], x[i + 1]);
+            }
+
+            pictureBox1.Invalidate();
+        }
+
+        private void radioButton_polygon_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_polygon.Checked)
+                polygons.Add(new List<Point>());
+            else
+                if (polygons[polygons.Count - 1].Count == 0)
+            {
+                polygons.RemoveAt(polygons.Count - 1);
+            }
+            else
+                cur_pol = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            graph.Clear(Color.White);
+            radioButton_point.Checked = true;
+            polygons.Clear();
+            points.Clear();
+            lines.Clear();
+            polygon_count = 0; point_count = 0; lines_count = 0;
+            pictureBox1.Invalidate();
+            curpol = "";
+            curpoint = "";
+            curline1 = "";
+            curline2 = "";
+            
         }
     }
 }
